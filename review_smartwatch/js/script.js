3e9b9949f707492e44894ff67ffa15ba0da06ee6
Leandro Sousa <133065183+LeoSousa23@users.noreply.github.com>
2024-02-21 21:49:00 -0300

    add function open close modal

--- review_smartwatch/js/script.js
+++ review_smartwatch/js/script.js
@@ -1,7 +1,10 @@
 const firstScreen = document.querySelector('#first-screen')
 const secondScreen = document.querySelector('#second-screen')
+const modal = document.querySelector('#modal-wrapper')
 const btnContinue = document.querySelector('#btn-continue')
 const btnToGoBack = document.querySelector('#togoback')
+const btnSend = document.querySelector('#btn-send')
+const btnCloseModal = document.querySelector('#close')
 
 function toggleScreen() {
   firstScreen.classList.toggle('hide')
@@ -12,6 +15,17 @@ function handleClick() {
   toggleScreen()
 }
 
+function openModal() {
+  modal.classList.add('open')
+}
+
+function closeModal() {
+  modal.classList.remove('open')
+  window.location.reload()
+}
+
 // pegando os eventos de click nos botoes
 btnContinue.addEventListener('click', handleClick)
 btnToGoBack.addEventListener('click', handleClick)
+btnSend.addEventListener('click', openModal)
+btnCloseModal.addEventListener('click', closeModal)
